b26c5cbfa3a26758e015b6401931d8ed5d8fdd57
basednai <naiym.harris@icloud.com>
2025-01-11 21:55:00 -0500

    vercel test

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://linear-progression-backend.vercel.app",
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
