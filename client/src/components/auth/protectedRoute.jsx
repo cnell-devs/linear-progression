6e233433eed4970ed22374db3f9fd7f146b5c8d1
basednai <naiym.harris@icloud.com>
2025-01-12 04:04:51 -0500

    refresh fix

--- client/src/components/auth/protectedRoute.jsx
+++ client/src/components/auth/protectedRoute.jsx
@@ -7,9 +7,9 @@ export const ProtectedRoute = ({ children }) => {
   const { user } = useAuth(); // Access authentication state
 
   // Prevent rendering until user is determined
-  if (user === undefined ) {
-    return <div>Loading...</div>; // Replace with a spinner or placeholder
-  }
+  // if (user === undefined ) {
+  //   return <div>Loading...</div>; // Replace with a spinner or placeholder
+  // }
 
   if (!user) {
     return <Navigate to="/login" replace />; // Redirect to login if not authenticated
