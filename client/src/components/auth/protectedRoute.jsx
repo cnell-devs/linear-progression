459efeae2f6778d3e1e08f1a312c4c27ac8bc81e
basednai <naiym.harris@icloud.com>
2025-01-12 03:55:38 -0500

    refresh fix

--- client/src/components/auth/protectedRoute.jsx
+++ client/src/components/auth/protectedRoute.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./authContext"; // Assuming AuthContext is already set up
+import { useEffect } from "react";
 
 export const ProtectedRoute = ({ children }) => {
   const { user } = useAuth(); // Access authentication state
+  useEffect(() => {
 
-  setTimeout(() => {
     if (!user) {
       return <Navigate to="/login" replace />; // Redirect to login if not authenticated
     }
+  }, [user]);
 
-    return children;
-  }, 1000);
+  return children;
 };
